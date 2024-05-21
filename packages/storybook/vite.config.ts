51b1a0df1fe2b97e209ec859c4c3c55dd2480093
x1ngYu <handsome.rxy@gmail.com>
2024-05-21 18:27:31 +0800

    fix: vanilla don't recomplie after updated .css.ts

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
-export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.css'],
-  },
+export default defineConfig((config) => {
+  return {
+    plugins: [
+      react(),
+      vanillaExtractPlugin({
+        unstable_mode: config.mode === 'development' ? 'transform' : 'emitCss',
+      }),
+    ],
+    resolve: {
+      extensions: [
+        '.mjs',
+        '.js',
+        '.mts',
+        '.ts',
+        '.jsx',
+        '.tsx',
+        '.json',
+        '.css',
+      ],
+    },
+  }
 })
