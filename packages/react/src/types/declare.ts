20f4415ce401c30536b3aca2cc32a254c5455cda
x1ngYu <handsome.rxy@gmail.com>
2025-05-16 17:34:03 +0800

    fix: fix the sx prop's incorrect type hints

--- packages/react/src/types/declare.ts
+++ packages/react/src/types/declare.ts
@@ -5,11 +5,19 @@ import type { Breakpoints as BreakpointsOverrides } from './generated/breakpoint
 import type { Selectors as SelectorsOverrides } from './generated/selectors'
 import type { Aliases as AliasesOverrides } from './generated/aliases'
 
+// Prevent rollup-plugin-ts from optimizing away type aliases during the bundling process.
+type _TokensOverrides = TokensOverrides
+type _SemanticTokensOverrides = SemanticTokensOverrides
+type _ScalesOverrides = ScalesOverrides
+type _BreakpointsOverrides = BreakpointsOverrides
+type _SelectorsOverrides = SelectorsOverrides
+type _AliasesOverrides = AliasesOverrides
+
 declare module '@nex-ui/system' {
-  interface Aliases extends AliasesOverrides {}
-  interface Selectors extends SelectorsOverrides {}
-  interface Breakpoints extends BreakpointsOverrides {}
-  interface Scales extends ScalesOverrides {}
-  interface SemanticTokens extends SemanticTokensOverrides {}
-  interface Tokens extends TokensOverrides {}
+  interface Aliases extends _AliasesOverrides {}
+  interface Selectors extends _SelectorsOverrides {}
+  interface Breakpoints extends _BreakpointsOverrides {}
+  interface Scales extends _ScalesOverrides {}
+  interface SemanticTokens extends _SemanticTokensOverrides {}
+  interface Tokens extends _TokensOverrides {}
 }
