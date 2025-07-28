9b33aca8dfc0ec59ff89623e0a19b29e3ab9213f
x1ngYu <25836768@qq.com>
2025-07-28 15:49:29 +0800

    docs: update dialog component docs

---
+++ docs/content/_demo/dialog/index.tsx
@@ -0,0 +1,63 @@
+import usageCode from './Usage.demo?raw'
+import UsageDemo from './Usage.demo'
+import maxWidthsCode from './MaxWidths.demo?raw'
+import MaxWidthsDemo from './MaxWidths.demo'
+import fullScreenCode from './FullScreen.demo?raw'
+import FullScreenDemo from './FullScreen.demo'
+import placementsCode from './Placements.demo?raw'
+import PlacementsDemo from './Placements.demo'
+import hideBackdropCode from './HideBackdrop.demo?raw'
+import HideBackdropDemo from './HideBackdrop.demo'
+import PreventScrollCode from './PreventScroll.demo?raw'
+import PreventScrollDemo from './PreventScroll.demo'
+import KeepMountedCode from './KeepMounted.demo?raw'
+import KeepMountedDemo from './KeepMounted.demo'
+import controlledCode from './Controlled.demo?raw'
+import ControlledDemo from './Controlled.demo'
+import scrollBehaviorCode from './ScrollBehavior.demo?raw'
+import ScrollBehaviorDemo from './ScrollBehavior.demo'
+import customMotionCode from './CustomMotion.demo?raw'
+import CustomMotionDemo from './CustomMotion.demo'
+
+export const dialog = {
+  usage: {
+    code: usageCode,
+    demo: <UsageDemo />,
+  },
+  maxWidths: {
+    code: maxWidthsCode,
+    demo: <MaxWidthsDemo />,
+  },
+  fullScreen: {
+    code: fullScreenCode,
+    demo: <FullScreenDemo />,
+  },
+  placements: {
+    code: placementsCode,
+    demo: <PlacementsDemo />,
+  },
+  hideBackdrop: {
+    code: hideBackdropCode,
+    demo: <HideBackdropDemo />,
+  },
+  preventScroll: {
+    code: PreventScrollCode,
+    demo: <PreventScrollDemo />,
+  },
+  keepMounted: {
+    code: KeepMountedCode,
+    demo: <KeepMountedDemo />,
+  },
+  controlled: {
+    code: controlledCode,
+    demo: <ControlledDemo />,
+  },
+  scrollBehavior: {
+    code: scrollBehaviorCode,
+    demo: <ScrollBehaviorDemo />,
+  },
+  customMotion: {
+    code: customMotionCode,
+    demo: <CustomMotionDemo />,
+  },
+}
