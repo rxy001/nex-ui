2679ef70c3c35244ac41bce889e816fb675bb75e
x1ngYu <handsome.rxy@gmail.com>
2025-08-29 11:16:49 +0800

    feat: add icons

---
+++ packages/icons/src/components/outlined/CheckCircleOutlined.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { createIcon } from '../utils'
+
+export const CheckCircleOutlined = createIcon(
+  [
+    <path
+      fill='currentColor'
+      d='M16.03 10.03a.75.75 0 1 0-1.06-1.06l-4.47 4.47l-1.47-1.47a.75.75 0 0 0-1.06 1.06l2 2a.75.75 0 0 0 1.06 0z'
+      key='0'
+    />,
+    <path
+      fill='currentColor'
+      fillRule='evenodd'
+      d='M12 1.25C6.063 1.25 1.25 6.063 1.25 12S6.063 22.75 12 22.75S22.75 17.937 22.75 12S17.937 1.25 12 1.25M2.75 12a9.25 9.25 0 1 1 18.5 0a9.25 9.25 0 0 1-18.5 0'
+      clipRule='evenodd'
+      key='1'
+    />,
+  ],
+  'CheckCircleOutlined',
+)
