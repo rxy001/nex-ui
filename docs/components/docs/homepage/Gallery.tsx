dc19dfe5931e18dd53c71239712891dbcb25f0b4
x1ngYu <25836768@qq.com>
2025-09-16 15:33:15 +0800

    docs: preload avatar images in Gallery component

--- docs/components/docs/homepage/Gallery.tsx
+++ docs/components/docs/homepage/Gallery.tsx
@@ -1,10 +1,18 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import { preload } from 'react-dom'
 
 const ClientGallery = dynamic(
   () => import('./Gallery.client').then((m) => m.ClientGallery),
   { ssr: false },
 )
 
-export const Gallery = () => <ClientGallery />
+export const Gallery = () => {
+  preload('/avatars/avatar-1.jpg', { as: 'image' })
+  preload('/avatars/avatar-2.jpg', { as: 'image' })
+  preload('/avatars/avatar-3.webp', { as: 'image' })
+  preload('/avatars/avatar-4.jpg', { as: 'image' })
+
+  return <ClientGallery />
+}
