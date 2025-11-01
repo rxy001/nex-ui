9adcb5e076147bb4000a36ec46868aa3ae0d3d10
x1ngYu <25836768@qq.com>
2025-11-01 17:00:23 +0800

    feat: add Badge component

---
+++ packages/react/src/components/badge/__tests__/Badge.test.tsx
@@ -0,0 +1,87 @@
+import {
+  testClassNamesForwarding,
+  testColorDataAttrs,
+  testComponentStability,
+  testRefForwarding,
+  testRootClassName,
+  testSlotPropsForwarding,
+  testVariantDataAttrs,
+  renderWithNexUIProvider,
+} from '~/tests/shared'
+import { Badge } from '../index'
+import { badgeSlotClasses } from './classes'
+
+const slots = ['closeButton'] as const
+
+describe('Badge', () => {
+  testComponentStability(<Badge />)
+
+  testRootClassName(<Badge />)
+
+  testRefForwarding(<Badge />)
+
+  testColorDataAttrs(<Badge />)
+
+  testVariantDataAttrs(<Badge />, ['size', ['sm', 'md', 'lg', 'xl']])
+
+  testVariantDataAttrs(<Badge />, [
+    'radius',
+    ['none', 'sm', 'md', 'lg', 'xl', 'full'],
+  ])
+
+  testClassNamesForwarding(
+    <Badge closable />,
+    slots,
+    {
+      closeButton: 'test-close-button',
+    },
+    badgeSlotClasses,
+  )
+
+  testSlotPropsForwarding(
+    <Badge closable />,
+    slots,
+    {
+      closeButton: { className: 'test-close-button' },
+    },
+    badgeSlotClasses,
+  )
+
+  it('should render with default props', () => {
+    const { container } = renderWithNexUIProvider(<Badge>Badge</Badge>)
+    const badgeRoot = container.firstElementChild
+    expect(badgeRoot).toHaveClass(badgeSlotClasses.root)
+    expect(badgeRoot).toHaveAttribute('data-size', 'md')
+    expect(badgeRoot).toHaveAttribute('data-radius', 'md')
+    expect(badgeRoot).toHaveAttribute('data-variant', 'solid')
+    expect(badgeRoot).toHaveAttribute('data-color', 'blue')
+    expect(badgeRoot).toHaveAttribute('data-closable', 'false')
+    expect(badgeRoot).toHaveAttribute('data-disabled', 'false')
+
+    expect(badgeRoot).toMatchSnapshot()
+  })
+
+  it('should render close button when closable is true', async () => {
+    const handleClose = jest.fn()
+
+    const { getByLabelText, user } = renderWithNexUIProvider(
+      <Badge closable onClose={handleClose}>
+        Badge
+      </Badge>,
+    )
+    const closeButton = getByLabelText('Close Badge')
+    expect(closeButton).toBeInTheDocument()
+
+    await user.click(closeButton)
+
+    expect(handleClose).toHaveBeenCalledTimes(1)
+  })
+
+  it('should disable badge when disabled is true', () => {
+    const { container } = renderWithNexUIProvider(<Badge disabled>Badge</Badge>)
+    const badgeRoot = container.firstElementChild
+    expect(badgeRoot).toHaveAttribute('data-disabled', 'true')
+
+    expect(badgeRoot).toHaveStyleRule('pointer-events', 'none')
+  })
+})
---
+++ packages/react/src/components/badge/__tests__/classes.ts
@@ -0,0 +1,6 @@
+import { generateSlotClasses } from '../../utils'
+
+export const badgeSlotClasses = generateSlotClasses('nui-badge', [
+  'root',
+  'close-button',
+])
