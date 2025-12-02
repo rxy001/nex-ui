712e3157d8d70594bcc6349e48a88e0c4cea5303
x1ngYu <25836768@qq.com>
2025-12-02 17:30:57 +0800

    refactor: refactor the component structure of Dialog and Drawer

--- packages/react/src/components/modal/ModalPanel.tsx
+++ packages/react/src/components/modal/ModalPanel.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { useEvent } from '@nex-ui/hooks'
 import { defineRecipe } from '@nex-ui/system'
 import { useSlot } from '../utils'
 import { useModal } from './ModalContext'
-import type { ElementType, MouseEvent } from 'react'
+import type { ElementType } from 'react'
 import type { ModalPanelProps } from './types'
 
 const recipe = defineRecipe({
@@ -17,24 +16,25 @@ const recipe = defineRecipe({
 const style = recipe()
 
 export const ModalPanel = <RootComponent extends ElementType = 'div'>(
-  inProps: ModalPanelProps<RootComponent>,
+  props: ModalPanelProps<RootComponent>,
 ) => {
-  const { closeOnInteractOutside, setOpen } = useModal()
-
-  const props = inProps as ModalPanelProps
-
-  const handleClick = useEvent((e: MouseEvent<HTMLDivElement>) => {
-    if (closeOnInteractOutside && e.target === e.currentTarget) {
-      setOpen(false)
-    }
-  })
+  const { modalContentRef, setOpen, open, closeOnInteractOutside } = useModal()
 
   const [ModalPanelRoot, getModalPanelRootProps] = useSlot({
     style,
     elementType: 'div',
     externalForwardedProps: props,
     additionalProps: {
-      onClick: handleClick,
+      onClick: (event) => {
+        if (
+          open &&
+          closeOnInteractOutside &&
+          modalContentRef.current &&
+          !modalContentRef.current.contains(event.target as Node)
+        ) {
+          setOpen(false)
+        }
+      },
     },
   })
 
