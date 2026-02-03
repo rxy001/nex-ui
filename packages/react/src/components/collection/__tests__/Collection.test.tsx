2ff6f5b15c74bd4345a7c110d7940ac2201fc4a8
x1ngYu <25836768@qq.com>
2026-02-03 11:01:58 +0800

    feat: add Collection component

---
+++ packages/react/src/components/collection/__tests__/Collection.test.tsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react'
+import { CollectionItem, Collection } from '../index'
+import { useCollection } from '../useCollection'
+
+function TestCollection() {
+  const collection = useCollection()
+
+  return (
+    <Collection context={collection.context}>
+      <CollectionItem>
+        <button>Item 1</button>
+      </CollectionItem>
+      <CollectionItem>
+        <button>Item 2</button>
+      </CollectionItem>
+      <CollectionItem>
+        <button>Item 3</button>
+      </CollectionItem>
+    </Collection>
+  )
+}
+
+describe('Collection', () => {
+  it('should render collection items correctly', () => {
+    const { getByText } = render(<TestCollection />)
+
+    expect(getByText('Item 1')).toBeInTheDocument()
+    expect(getByText('Item 2')).toBeInTheDocument()
+    expect(getByText('Item 3')).toBeInTheDocument()
+  })
+})
