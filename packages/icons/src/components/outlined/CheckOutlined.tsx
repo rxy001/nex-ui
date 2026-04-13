2e337a5f9a3916fceb064e599ac162ab3d96904c
x1ngYu <25836768@qq.com>
2026-04-13 17:57:27 +0800

    chore: update check icon

--- packages/icons/src/components/outlined/CheckOutlined.tsx
+++ packages/icons/src/components/outlined/CheckOutlined.tsx
@@ -4,12 +4,12 @@ import { createIcon } from '../utils'
 
 export const CheckOutlined = createIcon(
   <path
-    fill='none'
-    stroke='currentColor'
     strokeLinecap='round'
     strokeLinejoin='round'
+    fill='none'
+    stroke='currentColor'
     strokeWidth='1.5'
-    d='m7 12.9l3.143 3.6L18 7.5'
+    d='M4 12 L9 17 L20 6'
   />,
   'CheckOutlined',
 )
